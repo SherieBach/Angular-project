164d79703f6359d6e5519cbca1b537339b4716fc
SherieBach <Sherie.klappenbach@gmail.com>
2019-06-01 19:00:41 +0200

    Unit test for get/add and delete items.

--- Angular.inl.3/MovieBase/src/app/services/cart.service.spec.ts
+++ Angular.inl.3/MovieBase/src/app/services/cart.service.spec.ts
@@ -1,12 +1,30 @@
-import { TestBed } from '@angular/core/testing';
-
+import { IMovie } from '../interfaces/IMovie';
 import { CartService } from './cart.service';
 
 describe('CartService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CartService;
+  const aMovie: IMovie = {id: 1, name: 'hej', price: 234, categoryId: 8, description: 'hej', imageUrl: 'jo'};
+
+  beforeEach(() => {
+    service = new CartService();
+    service.clearCart();
+  });
 
   it('should be created', () => {
-    const service: CartService = TestBed.get(CartService);
     expect(service).toBeTruthy();
+  });
+  it('should have an empty cart', () => {
+    expect(service.getCartItems().length).toBe(0); // expect cartItems array to be empty = 0
+    expect(service.getCartItems()).toEqual([]);  // equal to be same empty cartItems array
+
+  });
+  it('should contain an added movie', () => {
+    service.addToCart(aMovie);
+    expect(service.getCartItems()).toEqual([aMovie]);
+  });
+
+  it('should delete one item', () => {
+    service.deleteFromCart(aMovie.id);
+    expect(service.getCartItems()).toEqual([]);
   });
 });
