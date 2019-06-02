0aa99b0efb523f1106d1ff35fdf9f163c9e37e9b
SherieBach <Sherie.klappenbach@gmail.com>
2019-06-02 22:04:41 +0200

    Added cart service for search function

--- Angular.inl.3/MovieBase/src/app/products/products.component.ts
+++ Angular.inl.3/MovieBase/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {IMovie} from '../interfaces/IMovie';
 import {MovieService} from '../services/movie.service';
+import {CartService} from '../services/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -8,18 +9,17 @@ import {MovieService} from '../services/movie.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  inputValue = 'the';
-  // screenView = true;
+  inputValue: string;
   movie: IMovie;
   search: IMovie[];
 
-  constructor(private service: MovieService) {
+  constructor(private service: MovieService, private cartService: CartService) {
   }
 
   ngOnInit(): void {
   }
 
-  searchMovie(inputValue: string) {      // parameter som definerar värdet i function som ska sedan skriva ut användaren input.
+  searchMovie() {
     this.service.search(this.inputValue).subscribe((data) => {
       this.search = data;
     });
