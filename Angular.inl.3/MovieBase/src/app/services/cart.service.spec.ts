f9861f7968e1b0415a35df96f819c996e67f67c6
SherieBach <Sherie.klappenbach@gmail.com>
2019-06-12 22:59:34 +0200

    Added id order function

--- Angular.inl.3/MovieBase/src/app/services/cart.service.spec.ts
+++ Angular.inl.3/MovieBase/src/app/services/cart.service.spec.ts
@@ -13,18 +13,31 @@ describe('CartService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-  it('should have an empty cart', () => {
-    expect(service.getCartItems().length).toBe(0); // expect cartItems array to be empty = 0
-    expect(service.getCartItems()).toEqual([]);  // equal to be same empty cartItems array
-
-  });
-  it('should contain an added movie', () => {
-    service.addToCart(aMovie);
-    expect(service.getCartItems()).toEqual([aMovie]);
+  describe('getCartItems', () => {
+    it('should get the items in cart', () => {
+      expect(service.getCartItems().length).toBe(0); // expect cartItems array to be empty = 0
+      expect(service.getCartItems()).toEqual([]);  // equal to be same empty cartItems array
+    });
   });
 
-  it('should delete one item', () => {
-    service.deleteFromCart(aMovie.id);
-    expect(service.getCartItems()).toEqual([]);
+  describe('addToCart', () => {
+    it('should contain an added movie', () => {
+      service.addToCart(aMovie);
+      expect(service.getCartItems()).toEqual([aMovie]);
+    });
+  });
+  describe('deleteFromCart', () => {
+    it('should delete one item', () => {
+      service.deleteFromCart(aMovie.id);
+      expect(service.getCartItems()).toEqual([]);
+    });
+  });
+  describe('totalPrice', () => {
+    it('should post get the total price from cart', () => {
+      service.addToCart(aMovie);
+      expect(service.getCartItems()).toContain(aMovie);
+      service.getCartItems().reduce((cost, movie) => cost + aMovie.price, 0);
+      expect(aMovie.price).toBe(234);
+    });
   });
 });
--- Angular.inl.3/MovieBase/src/app/services/cart.service.ts
+++ Angular.inl.3/MovieBase/src/app/services/cart.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
 import {IMovie} from '../interfaces/IMovie';
+import {IOrderItem} from '../interfaces/IOrderItem';
+
 
 @Injectable({
   providedIn: 'root'
@@ -17,6 +19,25 @@ export class CartService {
     console.log('Here' + movie);
   }
 
+
+  perMovie(movies: IMovie[]): object {
+    const box = {};
+    movies.forEach((movie) => {
+      if (box[movie.id]) {
+        box[movie.id] = box[movie.id] + 1;
+      } else {
+        box[movie.id] = 1;
+      }
+    });
+    return box;
+  }
+
+  toOrderItems(movies: IMovie[]): IOrderItem[] {
+    const amountId = this.perMovie(movies);
+    const movieIds: string[] = Object.keys(amountId);
+    return movieIds.map((id => ({ProductId: parseInt(id, 10), Amount: amountId[id]})));
+  }
+
   getCartItems(): IMovie[] {
     const cartItems: IMovie[] = JSON.parse(localStorage.getItem('movies'));
     return cartItems ? cartItems : [];
@@ -29,6 +50,6 @@ export class CartService {
   }
 
   clearCart() {
-   localStorage.clear();
+    localStorage.clear();
   }
 }
